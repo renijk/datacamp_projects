89f37c52e6515d746d254c522fd0c98fde5a1701
renijk <71831945+renijk@users.noreply.github.com>
2024-12-21 19:59:41 +0100

    Migliorie
    Uso di f-strings, between() method, mode() restituiva già di suo un int.

--- investigating_netflix_movies.py
+++ investigating_netflix_movies.py
@@ -1,21 +1,22 @@
-# Importing pandas and matplotlib
+# Importing pandas
 import pandas as pd
 
 # Read in the Netflix CSV as a DataFrame
 netflix_df = pd.read_csv("netflix_data.csv")
 
-# Only selecting 90s movies -> netflix_90
+# Filter for 90s movies
 netflix_90 = netflix_df[
-    (netflix_df["release_year"] >= 1990) & 
-    (netflix_df["release_year"] <= 1999) & 
+    (netflix_df["release_year"].between(1990, 1999)) & 
     (netflix_df["type"] == "Movie")
 ]
-duration = int(netflix_90["duration"].mode()[0])
-print("Mode of duration (1990s movies): " + str(duration))
 
-# Counting short movies
+# Mode of duration for 90s movies
+duration = netflix_90["duration"].mode()[0]
+print(f"Mode of duration (1990s movies): {duration} minutes")
+
+# Count of short action movies (duration < 90 minutes)
 short_movie_count = netflix_90[
-  (netflix_90["duration"] < 90) & 
-  (netflix_90["genre"] == 'Action')
-  ].shape[0]
-print("Number of short action movies: " + str(short_movie_count))
+    (netflix_90["duration"] < 90) & 
+    (netflix_90["genre"] == 'Action')
+].shape[0]
+print(f"Number of short action movies: {short_movie_count}")
