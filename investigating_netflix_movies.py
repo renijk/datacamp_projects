fa48349b7ac5f555db00eedd460bb38745095650
renijk <71831945+renijk@users.noreply.github.com>
2024-12-21 19:36:30 +0100

    Create investigating_netflix_movies.py
    Prima soluzione

---
+++ investigating_netflix_movies.py
@@ -0,0 +1,22 @@
+# Importing pandas and matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read in the Netflix CSV as a DataFrame
+netflix_df = pd.read_csv("netflix_data.csv")
+
+# Only selecting 90s movies -> netflix_90
+netflix_90 = netflix_df[
+    (netflix_df["release_year"] >= 1990) & 
+    (netflix_df["release_year"] <= 1999) & 
+    (netflix_df["type"] == "Movie")
+]
+duration = int(netflix_90["duration"].mode()[0])
+print("Mode of duration (1990s movies): " + str(duration))
+
+# Counting short movies
+short_movie_count = netflix_90[
+  (netflix_90["duration"] < 90) & 
+  (netflix_90["genre"] == 'Action')
+  ].shape[0]
+print("Number of short action movies: " + str(short_movie_count))
